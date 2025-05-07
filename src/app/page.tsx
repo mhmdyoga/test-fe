57172ae1de6540c5c9393588420cb8414cca3ddf
yogacahayq <yogacahayq@gmail.com>
2025-05-07 13:15:52 +0000

    fix-error_wrapped_main_page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
 import Homepage from "@/components/pages/Homepage";
+import { Loader } from "lucide-react";
+import { Suspense } from "react";
 
 export default function Home() {
   return (
     <div>
+      <Suspense fallback={<Loader className="w-10 h-10 animate-spin"/>}>
       <Homepage/>
+      </Suspense>
     </div>
   );
 }
